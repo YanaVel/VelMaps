62dcf3ce439df020bd96262ec4bb1a0c4ca5ba2d
YanaVel <iana.velich@gmail.com>
2017-11-11 16:31:51 +0200

    Add TestClass

---
+++ TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  VelMaps
+//
+//  Created by Yana Velichkevych on 11/11/17.
+//  Copyright © 2017 cat_lus. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+
+}
